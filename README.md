d1513bab04bd110acca8648a3380e501189d9d3b
Juan Sebastian Angarita <alosoleil@gmail.com>
2013-03-10 22:08:15 -0700

    Cleared repo for new version

--- README.md
+++
@@ -1,2 +0,0 @@
-#This is the repo for the deltaFunction website.
-#Ruby on Rails
--- test/functional/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionController::TestCase
-  test "should get render" do
-    get :render
-    assert_response :success
-  end
-
-end
