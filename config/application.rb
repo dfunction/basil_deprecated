c7013d617653cec164f29846b99540c2cb34031d
Juan Sebastian Angarita <alosoleil@gmail.com>
2013-07-07 13:58:38 -0700

    Fixing assets for production

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Basil
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.css_compressor = :yui
+    config.assets.js_compressor = :uglifier
   end
 end
